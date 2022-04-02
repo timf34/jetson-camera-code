948133559c2dd29208b2f7b42810bd04a0327bdc
timf34 <66926418+timf34@users.noreply.github.com>
2022-04-02 23:53:10 +0100

    Update stream_port1234.sh

--- stream_port1234.sh
+++ stream_port1234.sh
@@ -1,3 +1,3 @@
 gst-launch-1.0 -ev nvarguscamerasrc ! nvv4l2h264enc insert-vui=1 ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=192.168.118.141 port=1234 
 
-# in the file above, host must equal the IP adress of your laptop!!! Testing test 12345
+# in the file above, host must equal the IP adress of your laptop!!!
