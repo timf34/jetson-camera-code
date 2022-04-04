f3342d791c1a65fc92e366ecd6c94893cbd64399
timf34 <farrelti@tcd.ie>
2022-04-04 22:47:10 +0100

    initial commit

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 class BohsConfig:
     def __init__(self):
-        self.hour : int = 17
+        self.hour : int = 18
         self.minute : int = 45
         self.second : int = 1
         self.microsecond : int = 1
