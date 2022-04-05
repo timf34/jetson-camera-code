f4678cb4282a5f12b73ee1e28a855ec66dffa57b
timf34 <66926418+timf34@users.noreply.github.com>
2022-04-05 01:23:31 +0100

    Testing jetson update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 class BohsConfig:
     def __init__(self):
-        self.hour : int = 18
+        self.hour : int = 16
         self.minute : int = 45
         self.second : int = 1
         self.microsecond : int = 1
