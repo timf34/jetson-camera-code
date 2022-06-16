c63647185bf8a0053850413423fef95963d89b77
timf34 <farrelti@tcd.ie>
2022-06-16 19:01:58 +0100

    initial commit

---
+++ bohs_net_code/augs.py
@@ -0,0 +1,20 @@
+import numpy
+import cv2
+import torchvision.transforms as transforms
+
+NORMALIZATION_MEAN = [0.485, 0.456, 0.406]
+NORMALIZATION_STD = [0.229, 0.224, 0.225]
+
+normalize_trans = transforms.Compose([transforms.ToTensor(),
+                                      transforms.Normalize(NORMALIZATION_MEAN, NORMALIZATION_STD)])
+
+def image2tensor(image):
+    # Convert PIL Image to the tensor (with normalization)
+    return normalize_trans(image)
+
+
+def numpy2tensor(image):
+    # Convert OpenCV image to tensor (with normalization)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(image)
+    return image2tensor(pil_image)
