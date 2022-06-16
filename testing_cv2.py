f89ce901155123c4641dde2166105126d28a955a
timf34 <farrelti@tcd.ie>
2022-06-16 22:02:31 +0100

    cv2 working on jetson locally

--- testing_cv2.py
+++ testing_cv2.py
@@ -1,6 +1,20 @@
 import cv2
+print(cv2.__version__)
+dispW=1920
+dispH=1080
+flip=2
 
+cap = cv2.VideoCapture('nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink')
 
-if __name__ == '__main__':
-    print(cv2.__version__)
-    
+if cap.isOpened():
+    print("cap.isOpened:", cap.isOpened())
+    cv2.namedWindow("demo", cv2.WINDOW_AUTOSIZE)
+    while True:
+        ret_val, img = cap.read();
+        if not ret_val:
+            break
+        cv2.imshow('demo',img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+else:
+    print ("camera open failed")
