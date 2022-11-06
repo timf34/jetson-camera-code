c439699ba0016ca61a4e5dd23fa10253a453f6c3
timf34 <66926418+timf34@users.noreply.github.com>
2022-11-06 14:56:51 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 class BohsConfig:
     def __init__(self):
-        self.hour: int = 19
+        self.hour: int = 15
         self.minute: int = 00
         self.second: int = 1
         self.microsecond: int = 1
