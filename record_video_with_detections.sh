4e9c9a130952863b93c02cec459edd415478e096
timf34 <farrelti@tcd.ie>
2022-11-10 12:58:19 -0500

    added comment and mvoed record_video_with_detections.sh to scripts directory

--- record_video_with_detections.sh
+++ record_video_with_detections.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Note: I was having issues running this file at Bohs on 6/11/22 (before I changed directory)
+
 dt=$(date '+%d_%m_%Y');
-mkdir -p ./logs
-python3 record_and_detect_main_loop.py 2>&1 | tee logs/"$dt".txt
+mkdir -p ../logs
+python3 ../record_and_detect_main_loop.py 2>&1 | tee ../logs/"$dt".txt
