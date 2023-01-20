f9eced2a5fec9dcdd22c347c2fb8f17da01de937
timf34 <farrelti@tcd.ie>
2023-01-20 21:00:54 +0000

    working in capturing an image

--- send_image/send_image.py
+++ send_image/send_image.py
@@ -1,8 +1,17 @@
+import paramiko
 import subprocess
 
 # GStreamer pipeline to capture and send the image
 # This pipeline takes and image. It uses multifilesinks to take multiple images to allow auto-tune to warm up; we only take the last one with a time buffer of 1,000,000 nanoseconds (1 second).
-pipeline = "gst-launch-1.0 nvarguscamearsrc sensor_id=0\" ! \"video/x-raw(memory:NVMM), width=1920, height=1080, framerate=60/1\" ! nvjpegenc ! multifilesink location=test_yolo.jpg max-files=1 max-file-duration=1000000000"
+# Note that the early timeout is crucial in getting the stream to turn off.
+pipeline = "gst-launch-1.0 nvarguscamerasrc sensor_id=0 timeout=10 ! \"video/x-raw(memory:NVMM), width=1920, height=1080, framerate=60/1\" ! nvjpegenc ! multifilesink location=test_yolo.jpg max-files=1 max-file-duration=1000000000"
 
-# Run the pipeline
-subprocess.call(pipeline, shell=True)
+# # Run the pipeline
+# subprocess.run(pipeline)
+
+# pipeline = "echo \"hello world\""
+
+subprocess.run(pipeline, shell=True)
+
+
+# Send the image to our laptop
