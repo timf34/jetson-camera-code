b310b12ae64141bddb8f8744ca41e8fd61c60202
timf34 <farrelti@tcd.ie>
2023-01-20 22:04:35 +0000

    sending image to windows laptop works!

--- send_image/send_image.py
+++ send_image/send_image.py
@@ -1,17 +1,32 @@
 import paramiko
 import subprocess
 
+from windows_config import PASSWORD
+
 # GStreamer pipeline to capture and send the image
 # This pipeline takes and image. It uses multifilesinks to take multiple images to allow auto-tune to warm up; we only take the last one with a time buffer of 1,000,000 nanoseconds (1 second).
 # Note that the early timeout is crucial in getting the stream to turn off.
-pipeline = "gst-launch-1.0 nvarguscamerasrc sensor_id=0 timeout=10 ! \"video/x-raw(memory:NVMM), width=1920, height=1080, framerate=60/1\" ! nvjpegenc ! multifilesink location=test_yolo.jpg max-files=1 max-file-duration=1000000000"
+# pipeline = "gst-launch-1.0 nvarguscamerasrc sensor_id=0 timeout=10 ! \"video/x-raw(memory:NVMM), width=1920, height=1080, framerate=60/1\" ! nvjpegenc ! multifilesink location=test_yolo.jpg max-files=1 max-file-duration=1000000000"
 
-# # Run the pipeline
-# subprocess.run(pipeline)
-
-# pipeline = "echo \"hello world\""
-
-subprocess.run(pipeline, shell=True)
+# # # Run the pipeline
+# subprocess.run(pipeline, shell=True)
 
 
 # Send the image to our laptop
+# Set up the SSH client
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# Connect to the Windows machine
+# Connect to the Windows machine
+client.connect(hostname='192.168.84.1', username='timf34/timf3', password=PASSWORD)
+
+# Open a SFTP session
+sftp = client.open_sftp()
+
+# Send the file
+sftp.put('test_yolo.jpg', 'C:/Users/timf3/Downloads/test_yolo.jpg')
+
+# Close the SFTP session and SSH client
+sftp.close()
+client.close()
