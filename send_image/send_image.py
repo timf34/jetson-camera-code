cdd27973df3b26181e641710b3007025a4306ead
timf34 <farrelti@tcd.ie>
2023-01-23 11:44:41 +0000

    made send_image a function; adjusted so it just runs image script, no sending

--- send_image/send_image.py
+++ send_image/send_image.py
@@ -3,30 +3,40 @@ import subprocess
 
 from windows_config import PASSWORD
 
+LAPTOP_IPV4_ADDRESS: str = '192.168.84.1'
+LAPTOP_USERNAME: str = 'timf34/timf3'  # found via `whoami` on the windows machine
+
 # GStreamer pipeline to capture and send the image
 # This pipeline takes and image. It uses multifilesinks to take multiple images to allow auto-tune to warm up; we only take the last one with a time buffer of 1,000,000 nanoseconds (1 second).
 # Note that the early timeout is crucial in getting the stream to turn off.
-# pipeline = "gst-launch-1.0 nvarguscamerasrc sensor_id=0 timeout=10 ! \"video/x-raw(memory:NVMM), width=1920, height=1080, framerate=60/1\" ! nvjpegenc ! multifilesink location=test_yolo.jpg max-files=1 max-file-duration=1000000000"
+pipeline = "gst-launch-1.0 nvarguscamerasrc sensor_id=0 timeout=10 ! \"video/x-raw(memory:NVMM), width=1920, height=1080, framerate=60/1\" ! nvjpegenc ! multifilesink location=test_yolo2.jpg max-files=1 max-file-duration=1000000000"
 
 # # # Run the pipeline
-# subprocess.run(pipeline, shell=True)
+subprocess.run(pipeline, shell=True)
 
 
-# Send the image to our laptop
-# Set up the SSH client
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+def send_image():
+    """
+    Send an image from our jetson nano to our laptop
+    """
 
-# Connect to the Windows machine
-# Connect to the Windows machine
-client.connect(hostname='192.168.84.1', username='timf34/timf3', password=PASSWORD)
+    # Set up the SSH client
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-# Open a SFTP session
-sftp = client.open_sftp()
+    # Connect to the Windows machine
+    client.connect(hostname=LAPTOP_IPV4_ADDRESS, username=LAPTOP_USERNAME, password=PASSWORD)
 
-# Send the file. Note that the target path needs to be to a file - a directory won't work!
-sftp.put('test_yolo.jpg', 'C:/Users/timf3/Downloads/test_yolo.jpg')
+    # Open a SFTP session
+    sftp = client.open_sftp()
 
-# Close the SFTP session and SSH client
-sftp.close()
-client.close()
+    # Send the file. Note that the target path needs to be to a file - a directory won't work!
+    sftp.put('test_yolo.jpg', 'C:/Users/timf3/Downloads/test_yolo.jpg')
+
+    # Close the SFTP session and SSH client
+    sftp.close()
+    client.close()
+
+
+# if __name__ == '__main__':
+#     send_image()
