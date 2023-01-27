581c1a6499e72f1b06bc2311463120e2c7c7ec12
timf34 <farrelti@tcd.ie>
2023-01-27 14:02:47 +0000

    cleaned up and refactored file

--- utils/utility_funcs.py
+++ utils/utility_funcs.py
@@ -5,27 +5,28 @@ import time
 
 from typing import Dict, List
 
-def get_ip_address():
-    """
-        Returns string - function annotations/ typing does weird stuff to code editor colours...?
-    """
-    # IP address 
+
+def get_ip_address() -> str:
+    """Get the IP address of the device"""
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    # print("The main IP address", s.getsockname()[0])
     return s.getsockname()[0]
 
-def save_to_json_file(data: Dict[str, List]) -> None:
-    # Create our filename based off the current time and date
+
+def save_to_json_file(data: Dict[str, List], dir: str = "../logs/jetson3") -> None:
+    """Save the data to a json file"""
     filename = time.strftime("%H%M-%d_%m_%Y") + ".json"
+    check_and_create_dir(dir)
+    full_path = os.path.join(dir, filename)  # Create the full path to the file
 
-    # Create the directory if it doesn't exist
-    if not os.path.exists("../logs/jetson3"):
-        os.makedirs("../logs/jetson3")
-
-    # Create the full path to the file
-    full_path = os.path.join("../logs/jetson3", filename)
-
-    # Save the data to the file
     with open(full_path, "w") as f:
         json.dump(data, f)
+
+
+def check_and_create_dir(dir_name: str) -> None:
+    """Check if the directory exists and create it if it doesn't"""
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+        print(f"Created directory: {dir_name}")
+    else:
+        print(f"Directory {dir_name} already exists")
