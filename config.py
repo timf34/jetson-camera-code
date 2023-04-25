fb8b54a25133ac55460957b927f96e269ce33551
timf34 <farrelti@tcd.ie>
2023-04-25 15:28:21 +0100

    updated time for Bohs on May 1

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 
 class BohsConfig:
     def __init__(self):
-        self.hour: int = 19
-        self.minute: int = 45
+        self.hour: int = 17
+        self.minute: int = 1
         self.second: int = 1
         self.microsecond: int = 1
 
