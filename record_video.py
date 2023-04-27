564b10b25fe8cf2497b6e17d60c08f66f31109ef
timf34 <farrelti@tcd.ie>
2023-04-27 19:01:07 +0100

    fully implemented record_full_match_in_batches; fixed video paths; general cleanup

--- record_video.py
+++ record_video.py
@@ -52,8 +52,6 @@ class VideoRecorder:
                 'nvvidconv ! video/x-raw, width=' + str(self.width) + ', height=' + str(self.height) + ', format=BGRx '
                 '! videoconvert ! video/x-raw, format=BGR ! appsink')  # Linux
 
-    # TODO: I might want to give this a path to save to, and not just a video name... idk. I could pass a full path
-    #  rather than just a video name
     def create_video_writer(self, video_name: str) -> cv2.VideoWriter:
         """Create a video writer object"""
         return cv2.VideoWriter(video_name,
@@ -75,27 +73,45 @@ class VideoRecorder:
         bohs_fps = FPS()
         return avg_fps, reading_fps, writing_fps, bohs_fps
 
-    def get_timeout(self, timeout_minute_length: float = 22.5) -> float:
-        """Return the time in minutes that the video should be recorded for (default is 22.5 minutes)"""
+    def get_timeout(self, timeout_minute_length: float = 22.5) -> int:
+        """Return the time in seconds that the video should be recorded for (default is 22.5 minutes)"""
         if self.debug is True:
-            return time.time() + 10  # 10-second video if in debug mode
+            return int(time.time() + 10)  # 10-second video if in debug mode
         else:
-            return time.time() + (timeout_minute_length * 60)
+            return int(time.time() + (timeout_minute_length * 60))
 
-    def record_video(self) -> None:
-        """Record and save a video for as long as set in the timeout"""
+    def get_video_path(self) -> str:
+        """Return the path to save the video to (doesn't include name!)"""
+        if os.name == 'nt':
+            return "./videos/"
+        elif self.debug is False:
+            video_name = self.create_datetime_video_name()
+
+            # TODO: implement a good naming procedure here.
+            return "../tim/bohsVids/" + self.today.strftime('%m_%d_%Y_tim@192.168.73.207')
+        else:
+            return "../tim/bohsVids/test"
+
+    def record_video(self, video_length_mins: float, video_path: str) -> None:
+        """
+        Record and save a video for as long as set in the timeout
+
+        :param video_length_mins: The length of the video in minutes
+        :param video_path: The path to save the video to (i.e. ./videos/) - does not include file name
+        """
         cap = self.get_capture()
-        video_name = self.create_datetime_video_name()
-        writer = self.create_video_writer(video_name=video_name)
 
+        video_name = self.create_datetime_video_name()
+        video_path = os.path.join(video_path, video_name) # Join the path and the name together
+
+        writer = self.create_video_writer(video_name=video_path)
         avg_fps, reading_fps, writing_fps, bohs_fps = self.initialize_fps_timers()
         frame_counter = 0
-        timeout = self.get_timeout(22.5)
+        timeout = self.get_timeout(video_length_mins)
 
         # Camera loop
         try:
             if cap.isOpened():
-                print(f"cap.isOpened: {cap.isOpened()}")
                 while cap.isOpened():
                     # Read the next frame
                     reading_fps.start()
@@ -134,27 +150,30 @@ class VideoRecorder:
         cv2.destroyAllWindows()
         print("Video saved to", video_name)
 
-    def get_vidoe_path(self) -> str:
-        # TODO: path isn't being used for saving the videos yet
 
-        # Ok so this function gets the path... it might as well set the video name too
-        if os.name == 'nt':
-            return "./videos/windows_video.avi"
-        elif self.debug is False:
-            video_name = self.create_datetime_video_name()
-
-            # TODO: implement a good naming procedure here.
-            return "../tim/bohsVids/" + self.today.strftime('%m_%d_%Y_tim@192.168.73.207')
-        else:
-            return "../tim/bohsVids/test"
+    @staticmethod
+    def wait_for_match_to_start(seconds_till_match: int) -> bool:
+        """Waits for the match to start"""
+        time_till_match_starts = time.time() + seconds_till_match
+        while time.time() < time_till_match_starts:
+            print("waiting for match to start")
+            time.sleep(1)
+        return True
 
     def record_full_match_in_batches(self) -> None:
-        """Records videos for the match. Four 22.5 minute long videos + one 10 minute long video for the halftime"""
+        """Records videos for the match. Four 22.5-minute long videos + one 10-minute long video for the halftime"""
         # Set our file directory
-        path = self.get_vidoe_path()
+        path = self.get_video_path()
         check_and_create_dir(path)
 
         seconds_till_match = self.get_seconds_till_match()
+        self.wait_for_match_to_start(seconds_till_match)  # Blocks until the match starts
+
+        for i in range(5):
+            if (i == 2) or (i > 5):
+                self.record_video(video_length_mins=10, video_path=path)
+            else:
+                self.record_video(video_length_mins=22.5, video_path=path)
 
     def run(self) -> None:
         """
@@ -169,7 +188,6 @@ class VideoRecorder:
         print(f"main ip address: {main_ip_address}")
 
         seconds_till_match = self.get_seconds_till_match()
-        print("the match begins in ", seconds_till_match, " seconds")
 
         _timeout = time.time() + seconds_till_match
 
@@ -189,4 +207,4 @@ class VideoRecorder:
 
 if __name__ == '__main__':
     video_recorder = VideoRecorder(debug=True)
-    video_recorder.run()
+    video_recorder.record_full_match_in_batches()
